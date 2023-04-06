474d7a90d8c76c6738b27ea6adc54c9419e4a43f
Chris <chris.kamau@student.moringaschool.com>
2023-04-06 14:56:05 +0300

    Customer Feedback Form Added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function displayLiquor(liquor) {
         <h5 class="card-brand">Country of origin: ${liquor.origin}</h5>
         <h5 class="card-brand">Type: ${liquor.brand}</h5>
         <p class="card-text"> Available quantity is ${liquor.quantity}</p>
-        <a href="#" class="btn btn-primary">Add to cart</a>
+        <a href="#" class="btn btn-warning">Add to cart</a>
       </div>
     `
   })
@@ -47,4 +47,27 @@ fetch("http://localhost:5000/liquors")
 }
 
 fetchLiquors()
+
+
+// Function to handle form submission
+function handleFormSubmit(event) {
+  event.preventDefault()
+
+  // Clear input values
+  document.querySelector("#name").value = ""
+  document.querySelector("#email").value = ""
+  document.querySelector("#message").value = ""
+
+  // Display success message
+  const successMsg = document.createElement("div")
+  successMsg.classList.add("alert", "alert-success")
+  successMsg.setAttribute("role", "alert")
+  successMsg.textContent = "Thank you for your feedback!"
+  document.querySelector("#formId").append(successMsg)
+
+  // Send form data to server (not implemented)
+}
+
+document.querySelector("#formId").addEventListener("submit", handleFormSubmit)
+
 
